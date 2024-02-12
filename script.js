16b09ef89c89c7c4373b21339a0c3612c06838dd
mikesternk <mikesternk@gmail.com>
2024-02-12 14:49:17 -0600

    Adding question details

---
+++ script.js
@@ -0,0 +1,10 @@
+// Constants given for assignment
+const PI = 3.1415;
+const radius = 5; // in meters
+const area = PI * radius * radius;
+
+// each plant -> minimum space of 0.8meters
+
+// area starts with 20 plants
+
+// numOf plants double every week
